94f142bcc73eb993ab95399b263b9aaabf7a909e
Matt Wynne <matt@mattwynne.net>
2020-02-06 22:35:19 +0000

    Add `rake code` to unroll chapter code from a local branch

---
+++ tasks/languages.rb
@@ -0,0 +1,17 @@
+LANGUAGES = {
+  ruby: "Ruby",
+  js:   "JavaScript"
+}
+
+task :list do
+  puts "Available languages: \n"
+
+  LANGUAGES.each do |code, name|
+    puts " rake \"html[#{lang}]\"  # Generate HTML for #{name} version."
+  end
+end
+
+def language_codes(args = {})
+  Array(args[:lang] || LANGUAGES.keys)
+end
+
