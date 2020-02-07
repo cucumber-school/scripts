bb32b71b78c66de4d57480714aaabba1f32e255f
Seb Rose <seb@claysnow.co.uk>
2020-02-07 12:32:52 +0000

    Added step definition

---
+++ shouty/src/test/java/shouty/StepDefinitions.java
@@ -0,0 +1,25 @@
+package shouty;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+    @Given("Lucy is {int} metres from Sean")
+    public void lucy_is_metres_from_Sean(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("Sean shouts {string}")
+    public void sean_shouts(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Lucy should hear Sean's message")
+    public void lucy_should_hear_Sean_s_message() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
