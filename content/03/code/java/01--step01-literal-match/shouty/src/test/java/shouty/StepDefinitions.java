05d636427fe24c3647f3d8808e1ee84c5bd577f1
Seb Rose <seb@claysnow.co.uk>
2020-05-13 14:52:55 +0100

    Added code for Java Ch03

---
+++ content/03/code/java/01--step01-literal-match/shouty/src/test/java/shouty/StepDefinitions.java
@@ -0,0 +1,31 @@
+package shouty;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+public class StepDefinitions {
+    Person lucy = new Person();
+    Person sean = new Person();
+    private String messageFromSean;
+
+    @Given("Lucy is located 15 metres from Sean")
+    public void lucy_is_metres_from_Sean() {
+        throw new PendingException("Matched!");
+    }
+
+    @When("Sean shouts {string}")
+    public void sean_shouts(String message) {
+        sean.shout(message);
+        messageFromSean = message;
+     }
+
+    @Then("Lucy should hear Sean's message")
+    public void lucy_should_hear_Sean_s_message() {
+        assertEquals(asList(messageFromSean), lucy.getMessagesHeard());
+    }
+}
