91b8cad5e3c0aaf8095ec5d5b575627dd60c691d
Federico Iachetti <iachetti.federico@gmail.com>
2020-05-26 11:55:18 -0300

    Replace hardcoded lucy instance

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -2,10 +2,11 @@ require 'shouty'
 
 Before do
   @network = Shouty::Network.new
+  @people  = {}
 end
 
 Given('a person named Lucy') do
-  @lucy = Shouty::Person.new(@network)
+  @people['Lucy'] = Shouty::Person.new(@network)
 end
 
 Given('a person named Sean') do
@@ -18,5 +19,5 @@ When("Sean shouts {string}") do |message|
 end
 
 Then("Lucy should hear Sean's message") do
-  expect(@lucy.messages_heard).to eq [@message_from_sean]
+  expect(@people['Lucy'].messages_heard).to eq [@message_from_sean]
 end
