acea0ed94670f7d99d514418eb42d656cb0ca6df
Federico Iachetti <iachetti.federico@gmail.com>
2020-05-26 10:44:46 -0300

    Snippets added

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -8,6 +8,14 @@ Given("Lucy is {int} metres from Sean") do |distance|
   @lucy.move_to(distance)
 end
 
+Given('a person named Lucy') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('a person named Sean') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
 When("Sean shouts {string}") do |message|
   @sean.shout(message)
   @message_from_sean = message
