f27c3c4b5130d6cfa32ff5abe0d93def66ef06dd
Federico Iachetti <iachetti.federico@gmail.com>
2020-06-01 15:03:42 -0300

    Added Chapter 04 Ruby code

---
+++ content/04/code/ruby/00-initial-commit/Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+gem "cucumber", "~> 4.0.0.rc.6"
+gem "rspec-expectations", "~> 3.9.2"
---
+++ content/04/code/ruby/10-delete-move_to/features/step_definitions/steps.rb
@@ -0,0 +1,23 @@
+require 'shouty'
+
+Before do
+  @network = Shouty::Network.new
+end
+
+Given('a person named Lucy') do
+  @lucy = Shouty::Person.new(@network)
+end
+
+Given('a person named Sean') do
+  @sean = Shouty::Person.new(@network)
+end
+
+When("Sean shouts {string}") do |message|
+  sean = Shouty::Person.new(@network)
+  sean.shout(message)
+  @message_from_sean = message
+end
+
+Then("Lucy should hear Sean's message") do
+  expect(@lucy.messages_heard).to eq [@message_from_sean]
+end
