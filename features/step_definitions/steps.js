1edfd49335f1d0378aec67a6d7f07d0a3eac973e
Federico Iachetti <iachetti.federico@gmail.com>
2020-06-02 15:49:01 -0300

    Initial commit

---
+++ features/step_definitions/steps.js
@@ -0,0 +1,21 @@
+const { Given, When, Then } = require('@cucumber/cucumber')
+const { assertThat, is } = require('hamjest')
+
+const { Person, Network } = require('../../src/shouty')
+
+Given('Lucy is {int} metres from Sean', function (distance) {
+  this.network = new Network()
+  this.lucy    = new Person(this.network)
+  this.sean    = new Person(this.network)
+
+  this.lucy.moveTo(distance)
+})
+
+When('Sean shouts {string}', function (message) {
+  this.sean.shout(message)
+  this.messageFromSean = message
+})
+
+Then('Lucy should hear Sean\'s message', function () {
+  assertThat(this.lucy.messagesHeard(), is([this.messageFromSean]))
+})
