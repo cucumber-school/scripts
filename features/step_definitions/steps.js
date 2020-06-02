7ccec6c73a693f676804b9bbcfe7fdc977a5b1e1
Federico Iachetti <iachetti.federico@gmail.com>
2020-06-02 17:08:17 -0300

    Snippets added

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -11,6 +11,16 @@ Given('Lucy is {int} metres from Sean', function (distance) {
   this.lucy.moveTo(distance)
 })
 
+Given('a person named Lucy', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Given('a person named Sean', function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
 When('Sean shouts {string}', function (message) {
   this.sean.shout(message)
   this.messageFromSean = message
