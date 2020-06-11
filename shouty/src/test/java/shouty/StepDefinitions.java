98d505898b57615c52e93a266b37299c40709408
Seb Rose <seb@claysnow.co.uk>
2020-06-11 15:48:18 +0100

    ===Step07 Use cell() to demonstrate a 2D array

--- shouty/src/test/java/shouty/StepDefinitions.java
+++ shouty/src/test/java/shouty/StepDefinitions.java
@@ -44,14 +44,7 @@ public class StepDefinitions {
 
     @Given("people are located at")
     public void people_are_located_at(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Lucy's location: " + dataTable.cell(2 ,1));
     }
 
 
