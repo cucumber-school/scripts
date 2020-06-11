5f04996313e2d566c0dac9dad45a9aeeffa36cf2
Seb Rose <seb@claysnow.co.uk>
2020-06-11 17:25:21 +0100

    ===Step13 Outcome data tables

--- shouty/src/test/java/shouty/StepDefinitions.java
+++ shouty/src/test/java/shouty/StepDefinitions.java
@@ -87,4 +87,16 @@ public class StepDefinitions {
     public void larry_should_not_hear_a_shout() throws Throwable {
         assertEquals(0, people.get("Larry").getMessagesHeard().size());
     }
+
+    @Then("Lucy hears the following messages:")
+    public void lucy_hears_the_following_messages(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
 }
