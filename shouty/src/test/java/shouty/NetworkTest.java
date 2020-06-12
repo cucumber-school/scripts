d175201596f084983625b9b963fad65dbc484c5f
Seb Rose <seb@claysnow.co.uk>
2020-06-12 10:58:15 +0100

    ===Step19-Add test to NetworkTest

--- shouty/src/test/java/shouty/NetworkTest.java
+++ shouty/src/test/java/shouty/NetworkTest.java
@@ -2,6 +2,8 @@ package shouty;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -42,5 +44,20 @@ public class NetworkTest {
         network.broadcast(message, sallyLocation);
 
         verify(lionel, never()).hear(message);
+    }
+
+    @Test
+    public void does_not_broadcast_a_message_over_180_characters_even_if_listener_is_in_range() {
+        int seanLocation = 0;
+
+        char[] chars = new char[181];
+        Arrays.fill(chars, 'x');
+        String longMessage = String.valueOf(chars);
+
+        Person laura = mock(Person.class);
+        network.subscribe(laura);
+        network.broadcast(longMessage, seanLocation);
+
+        verify(laura, never()).hear(longMessage);
     }
 }
