dd4073d9c55d11ee52994b62b63fc6687b166c1b
Federico Iachetti <iachetti.federico@gmail.com>
2020-06-18 12:17:08 -0300

    Remove incidental details from range scenarios and fix step defs

--- features/hear_shout.feature
+++ features/hear_shout.feature
@@ -16,12 +16,12 @@ Feature: Hear shout
       Given the range is 100
       And a person named Sean is located at 0
       And a person named Lucy is located at 50
-      When Sean shouts "free bagels at Sean's"
-      Then Lucy should hear Sean's message
+      When Sean shouts
+      Then Lucy should hear a shout
 
     Scenario: Listener is out of range
       Given the range is 100
       And a person named Sean is located at 0
       And a person named Larry is located at 150
-      When Sean shouts "free bagels at Sean's"
-      Then Larry should not hear Sean's message
+      When Sean shouts
+      Then Larry should not hear a shout
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -19,6 +19,10 @@ Given "a person named {word} is located at {int}" do |name, location|
   @people[name] = Shouty::Person.new(@network, location)
 end
 
+When "Sean shouts" do
+  @people["Sean"].shout("Hello, world")
+end
+
 When "Sean shouts {string}" do |message|
   @people["Sean"].shout(message)
   @message_from_sean = message
@@ -28,6 +32,14 @@ Then "Lucy should hear Sean's message" do
   expect(@people['Lucy'].messages_heard).to eq [@message_from_sean]
 end
 
+Then "Lucy should hear a shout" do
+  expect(@people['Lucy'].messages_heard.count).to eq 1
+end
+
 Then "Larry should not hear Sean's message" do
   expect(@people['Larry'].messages_heard).not_to include(@message_from_sean)
+end
+
+Then "Larry should not hear a shout" do
+  expect(@people['Larry'].messages_heard.count).to eq 0
 end
