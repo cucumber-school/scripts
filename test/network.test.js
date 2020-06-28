2c8d699567679e7689afcd10332caf87efdb061b
Federico Iachetti <iachetti.federico@gmail.com>
2020-06-28 18:29:51 -0300

    Starting code

---
+++ test/network.test.js
@@ -0,0 +1,51 @@
+
+const assert = require('assert')
+const sinon = require('sinon')
+const {Person, Network} = require('../src/shouty')
+
+describe('Person', () => {
+
+  let network, message
+  beforeEach(() => {
+    const range   = 100
+    network = new Network(range)
+    const message = "Free bagels!"
+  })
+
+  it('broadcasts a message to a listener within range', function () {
+    const shouterLocation = 0
+    const listenerLocation = 90
+    const lucy = new Person(network, listenerLocation)
+    const lucyStub = sinon.spy(lucy)
+
+    network.subscribe(lucy)
+    network.broadcast(message, shouterLocation)
+
+    assert.strictEqual(lucyStub.hear.getCall(0).args[0], message);
+  })
+
+  it('does not broadcast a message to a listener out of range', function () {
+    const shouterLocation = 0
+    const listenerLocation = 150
+    const lucy = new Person(network, listenerLocation)
+    const lucyStub = sinon.spy(lucy)
+
+    network.subscribe(lucy)
+    network.broadcast(message, shouterLocation)
+
+    assert(lucyStub.hear.notCalled)
+  })
+
+  it('does not broadcast a message to a listener out of range negative distance', function () {
+    const shouterLocation = 0
+    const listenerLocation = -150
+    const lucy = new Person(network, listenerLocation)
+    const lucyStub = sinon.spy(lucy)
+
+    network.subscribe(lucy)
+    network.broadcast(message, shouterLocation)
+
+    assert(lucyStub.hear.notCalled)
+  })
+
+})
