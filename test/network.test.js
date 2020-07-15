6c01d7f5772d470cc9e4dfa941f6a043dd7a2d42
Federico Iachetti <iachetti.federico@gmail.com>
2020-07-15 19:40:47 -0300

    Add test to NetworkTest

--- test/network.test.js
+++ test/network.test.js
@@ -3,14 +3,11 @@ const assert = require('assert')
 const sinon = require('sinon')
 const {Person, Network} = require('../src/shouty')
 
-describe('Person', () => {
-
-  let network, message
-  beforeEach(() => {
-    const range   = 100
-    network = new Network(range)
-    const message = "Free bagels!"
-  })
+describe('Network', function () {
+  const range   = 100
+  const network = new Network(range)
+  const message = "Free bagels!"
+  const longMessage = 'x'.repeat(181)
 
   it('broadcasts a message to a listener within range', function () {
     const shouterLocation = 0
@@ -44,6 +41,18 @@ describe('Person', () => {
 
     network.subscribe(lucy)
     network.broadcast(message, shouterLocation)
+
+    assert(lucyStub.hear.notCalled)
+  })
+
+  it('does not broadcast a message over 180 characters even if listener is in range', function () {
+    const shouterLocation = 0
+    const listenerLocation = 90
+    const lucy = new Person(network, listenerLocation)
+    const lucyStub = sinon.spy(lucy)
+
+    network.subscribe(lucy)
+    network.broadcast(longMessage, shouterLocation)
 
     assert(lucyStub.hear.notCalled)
   })
