f17789146b9bbfdee3f72ce7d728f13684e7415a
Aslak Hellesøy <aslak.hellesoy@smartbear.com>
2020-07-22 11:20:11 +0100

    Implement Network test

---
+++ content/04/code/js/00-initial-commit/test/network.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert')
+const sinon = require('sinon')
+const {Network} = require('../src/shouty')
+
+describe('Network', () => {
+  it('broadcasts a message to all listeners', () => {
+    const network = new Network()
+    const message = "Free bagels!"
+    const lucy = { hear(message) {} }
+    const lucyMock = sinon.mock(lucy)
+    lucyMock.expects('hear').once().withArgs(message)
+    network.subscribe(lucy)
+    network.broadcast(message)
+    lucyMock.verify()
+  })
+})
