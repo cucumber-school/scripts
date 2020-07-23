6079233f46a9602b19ab85a2e63ef695f531ddb4
Seb Rose <seb@claysnow.co.uk>
2020-07-23 16:15:57 +0100

    Ported Ch5 unit tests to JS

--- content/05/code/js/20-tidy-up-network-logic/test/network.test.js
+++ content/05/code/js/20-tidy-up-network-logic/test/network.test.js
@@ -1,15 +1,7 @@
-// var assert = require('assert');
-// describe('Array', function () {
-//   describe('#indexOf()', function () {
-//     it('should return -1 when the value is not present', function () {
-//       assert.equal([1, 2, 3].indexOf(4), -1);
-//     });
-//   });
-// });
 
-const { assert } = require('assert')
-
-const { Person, Network } = require('../src/shouty')
+const assert = require('assert')
+const sinon = require('sinon')
+const {Person, Network} = require('../src/shouty')
 
 describe('Network', function () {
   const range   = 100
@@ -17,32 +9,39 @@ describe('Network', function () {
   const message = "Free bagels!"
 
   it('broadcasts a message to a listener within range', function () {
-    // let sean_location = 0
-    // let lucy = new Person
+    const shouterLocation = 0
+    const listenerLocation = 90
+    const lucy = new Person(network, listenerLocation)
+    const lucyStub = sinon.spy(lucy)
 
-    // network.subscribe(lucy)
-    // network.broadcast(message, sean_location)
+    network.subscribe(lucy)
+    network.broadcast(message, shouterLocation)
 
-    //  assert.equal([1, 2, 3].indexOf(4), -1);
-    // expect(lucy.messages_heard).to have_received(:hear).with(message)
+    assert.strictEqual(lucyStub.hear.getCall(0).args[0], message);
   })
 
   it('does not broadcast a message to a listener out of range', function () {
-    // sean_location = 0
-    // laura = spy(Shouty::Person, location: 101)
-    // network.subscribe(laura)
-    // network.broadcast(message, sean_location)
+    const shouterLocation = 0
+    const listenerLocation = 150
+    const lucy = new Person(network, listenerLocation)
+    const lucyStub = sinon.spy(lucy)
 
-    // expect(laura.messages_heard).not_to have_received(:hear).with(message)
+    network.subscribe(lucy)
+    network.broadcast(message, shouterLocation)
+
+    assert(lucyStub.hear.notCalled)
   })
 
   it('does not broadcast a message to a listener out of range negative distance', function () {
-    // sally_location = 101
-    // lionel = spy(Shouty::Person, location: 0)
-    // network.subscribe(lionel)
-    // network.broadcast(message, sally_location)
+    const shouterLocation = 0
+    const listenerLocation = -150
+    const lucy = new Person(network, listenerLocation)
+    const lucyStub = sinon.spy(lucy)
 
-    // expect(lionel.messages_heard).not_to have_received(:hear).with(message)
+    network.subscribe(lucy)
+    network.broadcast(message, shouterLocation)
+
+    assert(lucyStub.hear.notCalled)
   })
 
   it('does not broadcast a message over 180 characters even if listener is in range', function () {
