2198a20ae8f4916352721c7c1fd1502d571b4e80
Seb Rose <seb@claysnow.co.uk>
2020-09-29 12:53:19 +0100

    Output stepdef argument to console

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,8 +1,8 @@
 const { Given, When, Then } = require('@cucumber/cucumber')
 
-Given('Lucy is located {int} metres from Sean', function (int) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending'
+Given('Lucy is located {int} metres from Sean', function (distance) {
+  console.log(distance);
+  return 'pending';
 })
 
 When('Sean shouts {string}', function (string) {
