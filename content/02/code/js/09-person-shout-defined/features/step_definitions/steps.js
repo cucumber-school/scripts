94851c2e5f0d76084e5249bc39190a6e7c7e8a93
Seb Rose <seb@claysnow.co.uk>
2020-09-29 17:04:34 +0100

    Imnplemented Person.shout in an earlier commit

--- content/02/code/js/09-person-shout-defined/features/step_definitions/steps.js
+++ content/02/code/js/09-person-shout-defined/features/step_definitions/steps.js
@@ -7,9 +7,8 @@ Given('Lucy is located {int} metres from Sean', function (distance) {
   this.lucy.moveTo(distance)
 })
 
-When('Sean shouts {string}', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending'
+When('Sean shouts {string}', function (message) {
+  this.sean.shout(message)
 })
 
 Then('Lucy hears Sean\'s message', function () {
