de722703c1c02f19dee24a8cc9871a3788e7a451
Seb Rose <seb@claysnow.co.uk>
2021-02-02 13:07:55 +0000

    Remove logical tag filter

--- shouty/src/test/java/shouty/RunCucumberTest.java
+++ shouty/src/test/java/shouty/RunCucumberTest.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(tags="not @slow", plugin = {"pretty"}, strict = true)
+@CucumberOptions(plugin = {"pretty"}, strict = true)
 public class RunCucumberTest {
 }
