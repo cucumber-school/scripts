mastreips/bdd-with-cucumber

===Step19-Add test to NetworkTest

Shouty/Shouty.Tests/NetworkTests.cs
@@ -38,5 +38,18 @@ namespace Shouty.Tests
 
             Assert.DoesNotContain(Message, lionel.GetMessagesHeard());
         }
+        
+        [Fact]
+        public void Does_not_broadcast_a_message_over_180_characters_even_if_listener_is_in_range()
+        {
+            int seanLocation = 0;
+
+            var longMessage = new string('x', 181);
+
+            Person laura = new Person(network, 0);
+            network.Broadcast(longMessage, seanLocation);
+
+            Assert.DoesNotContain(Message, laura.GetMessagesHeard());
+        }
     }
 }

cucumber-school/scripts

Shouty/Shouty.Tests/NetworkTests.cs
@@ -38,5 +38,18 @@ namespace Shouty.Tests
 
             Assert.DoesNotContain(Message, lionel.GetMessagesHeard());
         }
+        
+        [Fact]
+        public void Does_not_broadcast_a_message_over_180_characters_even_if_listener_is_in_range()
+        {
+            int seanLocation = 0;
+
+            var longMessage = new string('x', 181);
+
+            Person laura = new Person(network, 0);
+            network.Broadcast(longMessage, seanLocation);
+
+            Assert.DoesNotContain(longMessage, laura.GetMessagesHeard());
+        }
     }
 }