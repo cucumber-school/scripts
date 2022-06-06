da1a8ae52b9a91070c225cb1308d9cb5a0b154ba
Matt Wynne <matt@cucumber.io>
2022-06-06 15:49:14 -0700

    Update 09.03.java commits

---
+++ content/bdd-with-cucumber/09/code/java/06-introduce-person-moveto-method/shouty/src/test/java/shouty/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package shouty;
+
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"}, strict = true, tags = "not @todo")
+public class RunCucumberTest {
+}
