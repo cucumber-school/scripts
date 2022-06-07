ef23d92a8ea97b1a71dbdde355855f57e10a9289
Matt Wynne <matt@cucumber.io>
2022-06-07 10:54:41 -0700

    Continue 09.java narrative

---
+++ content/bdd-with-cucumber/09/code/java/08-add-boilerplate-for-person-parameter-type/shouty/src/test/java/shouty/support/PersonParameterType.java
@@ -0,0 +1,12 @@
+package shouty.support;
+
+import io.cucumber.java.ParameterType;
+import shouty.Person;
+
+public class PersonParameterType {
+    @ParameterType("Lucy|Sean|Larry")
+    public Person person(String name) {
+        return null;
+    }
+}
+
---
+++ content/bdd-with-cucumber/09/code/java/10-inject-world-into-parameter-type-class/shouty/src/test/java/shouty/support/PersonParameterType.java
@@ -0,0 +1,18 @@
+package shouty.support;
+
+import io.cucumber.java.ParameterType;
+import shouty.Person;
+
+public class PersonParameterType {
+    private final ShoutyWorld world;
+
+    public PersonParameterType(ShoutyWorld world) {
+        this.world = world;
+    }
+
+    @ParameterType("Lucy|Sean|Larry")
+    public Person person(String name) {
+        return new Person(name, world.network, 0);
+    }
+}
+
