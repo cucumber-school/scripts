f790b02638b914ad4e80ea9b4d8ed0449bec1575
Matt Wynne <matt@cucumber.io>
2022-06-07 10:17:54 -0700

    Inject world into parameter type class

--- shouty/src/test/java/shouty/support/PersonParameterType.java
+++ shouty/src/test/java/shouty/support/PersonParameterType.java
@@ -4,6 +4,12 @@ import io.cucumber.java.ParameterType;
 import shouty.Person;
 
 public class PersonParameterType {
+    private final ShoutyWorld world;
+
+    public PersonParameterType(ShoutyWorld world) {
+        this.world = world;
+    }
+
     @ParameterType("Lucy|Sean|Larry")
     public Person person(String name) {
         return null;
