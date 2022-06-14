0245d7f1c4f8f49c883431ffefc7e94cf83f5cda
Matt Wynne <matt@cucumber.io>
2022-06-14 10:46:12 -0700

    extract shout steps

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -13,37 +13,6 @@ Given("{person} has bought {int} credits", function (person, credits) {
   person.credits = credits
 })
 
-When("{person} shouts", function (shouter) {
-  const message = "Hello, world"
-  this.shout({ from: shouter, message })
-})
-
-When(
-  "{person} shouts {int} messages containing the word {string}",
-  function (shouter, count, word) {
-    for (let i = 0; i < count; i++) {
-      const message = `A message containing the word ${word}`
-      this.shout({ from: shouter, message })
-    }
-  }
-)
-
-When("{person} shouts {int} over-long messages", function (shouter, count) {
-  for (let i = 0; i < count; i++) {
-    const baseMessage = `A message from ${shouter.name} that is 181 characters long `
-    const message = baseMessage + "x".repeat(181 - baseMessage.length)
-    this.shout({ from: shouter, message })
-  }
-})
-
-When("{person} shouts {string}", function (shouter, message) {
-  this.shout({ from: shouter, message })
-})
-
-When("{person} shouts the following message", function (shouter, message) {
-  this.shout({ from: shouter, message })
-})
-
 Then("{person} should not hear a shout", function (listener) {
   assertThat(listener.messagesHeard().length, is(0))
 })
