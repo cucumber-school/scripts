3aab1c51f0a99f94842d61e6634a0dbbf93d45a9
Matt Wynne <matt@cucumber.io>
2022-06-14 11:49:49 -0700

    extract shout steps

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -10,33 +10,6 @@ Given('{person} has bought {int} credits') do |person, credits|
   person.credits = credits
 end
 
-When "{person} shouts" do |shouter|
-  shout from: shouter, message: "Hello, world"
-end
-
-When('{person} shouts {string}') do |shouter, message|
-  shout from: shouter, message: message
-end
-
-When "{person} shouts the following message" do |shouter, message|
-  shout from: shouter, message: message
-end
-
-When "{person} shouts {int} over-long messages" do |shouter, count|
-  count.times do
-    base_message = "A message from #{shouter.name} that is 181 characters long "
-    message = base_message + "x" * (181 - base_message.size)
-    shout from: shouter, message: message
-  end
-end
-
-When '{person} shouts {int} messages containing the word {string}' do |shouter, count, word|
-  count.times do
-    message = "A message containing the word #{word}"
-    shout from: shouter, message: message
-  end
-end
-
 Then "{person} should hear a shout" do |listener|
   expect(listener.messages_heard.count).to eq 1
 end
