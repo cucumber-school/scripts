ba294f40cb87de99dba9d658d550afa6be768207
Federico Iachetti <iachetti.federico@gmail.com>
2022-06-22 14:50:11 -0300

    Updated script to work with branches instead of tags

--- tasks/tags.rb
+++
@@ -1,9 +0,0 @@
-task :tags do
-  refs = `git ls-remote --heads origin | grep refs/heads/code.`.lines.map { |line| line.split(" ").last.strip }
-  push_instructions = refs.map do |branch_ref|
-    tag_ref = "#{branch_ref.gsub("refs/heads/", "refs/tags/")}.start"
-    first_commit=`git rev-list --max-parents=0 #{branch_ref}`.strip
-    "#{first_commit}:#{tag_ref}"
-  end
-  system("git push origin #{push_instructions.join(" ")}")
-end
